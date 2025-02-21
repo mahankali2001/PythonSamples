2a58d9d74822907e6941aa6e53bab3e744b12527
mahankali <vijayakumar.mahankali@gmail.com>
2025-02-21 10:12:40 -0800

    linked list is cyclic or not - unit tests, test suite

---
+++ interviewp/linkedlistcycletest.py
@@ -0,0 +1,40 @@
+import unittest
+from linkedlistcycle import solution, node, linkedlist
+
+
+
+class linkedlistcycletest(unittest.TestCase):
+    def setUp(self):
+        self.solution = solution()
+	    
+    def test_llc1(self):
+        ll = linkedlist(1)
+        expected_output = False
+        self.assertEqual(self.solution.findCycleInLL(ll), expected_output)
+
+    def test_llc2(self):
+        ll = linkedlist(1)
+        n2 = node(2)
+        n3 = node(3)
+        n4 = node(4)
+        ll.head.next = n2
+        n2.next = n3
+        n3.next = n4
+        expected_output = False
+        self.assertEqual(self.solution.findCycleInLL(ll), expected_output)
+    
+    def test_llc3(self):
+        ll = linkedlist(1)
+        n2 = node(2)
+        n3 = node(3)
+        n4 = node(4)
+        ll.head.next = n2
+        n2.next = n3
+        n3.next = n4
+        n4.next = n2
+        expected_output = True
+        self.assertEqual(self.solution.findCycleInLL(ll), expected_output)
+
+if __name__ == "__main__":
+    # unittest.main()
+    unittest.main(argv=['first-arg-is-ignored', '-k', 'test_llc1'])
