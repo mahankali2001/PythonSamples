d65b3a1c2aa28c72ffbd1981a651cc3bf7fb84b3
mahankali <vijayakumar.mahankali@gmail.com>
2025-02-27 23:10:25 -0800

    string encode, decode - 3 versions

---
+++ neetcode/arrays-hash/string-encode-decode.py
@@ -0,0 +1,54 @@
+import base64
+
+class Solution:
+
+    def encode(self, strs: list[str]) -> str:
+        # str = ','.join(strs)
+        # return str
+        
+        str = ','.join(strs)
+        encoded_bytes = base64.b64encode(str.encode('utf-8'))
+        return encoded_bytes
+
+        # if not strs:
+        #     return ""
+        # sizes, res = [], ""
+        # for s in strs:
+        #     sizes.append(len(s))
+        # for sz in sizes:
+        #     res += str(sz)
+        #     res += ','
+        # res += '#'
+        # for s in strs:
+        #     res += s
+        # return res
+
+
+    def decode(self, s: str) -> list[str]:
+        # return s.split(",")
+        
+        decoded_str = base64.b64decode(s)
+        decoded_str = decoded_str.decode('utf-8')
+        return decoded_str.split(",")
+    
+        # if not s:
+        #     return []
+        # sizes, res, i = [], [], 0
+        # while s[i] != '#':
+        #     cur = ""
+        #     while s[i] != ',':
+        #         cur += s[i]
+        #         i += 1
+        #     sizes.append(int(cur))
+        #     i += 1
+        # i += 1
+        # for sz in sizes:
+        #     res.append(s[i:i + sz])
+        #     i += sz
+        # return res
+
+if __name__ == "__main__":
+    s = Solution()
+    str = s.encode(["neet","code","love","you"])
+    print(str)
+    print(s.decode(str))
