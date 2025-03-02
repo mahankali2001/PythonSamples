7a9879f976815ce0ae5b737dd145ae2015690a91
mahankali <vijayakumar.mahankali@gmail.com>
2025-03-02 12:30:54 -0800

    fix sudoku issues

--- neetcode/arrays-hash/Sudoku.py
+++ neetcode/arrays-hash/Sudoku.py
@@ -7,11 +7,12 @@ class Solution:
         for i, slist in enumerate(board):
             seen = set()
             for j, val in enumerate(slist):
+                print("row - {}, column - {}, value - {}".format(i, j,val))
                 if val == ".":
                     continue
                 if val in seen:
                     print(seen)
-                    print("found repetition in the row ({},{})".format(r,c))
+                    print("found repetition in the row - {}, column - {}".format(r,c))
                     return False
                 seen.add(val)
                 # sudokovals[(i,j)] = val
@@ -19,11 +20,12 @@ class Solution:
         for c in range(cols):
             seen = set()
             for r in range(rows):
+                print("column - {}, row - {}, value - {}".format(c, r, board[r][c]))
                 if board[r][c] == ".":
                     continue
                 if board[r][c] in seen:
                     print(seen)
-                    print("found repetition in the row ({},{})".format(r,c))
+                    print("found repetition in the column - {}, row - {}".format(c, r))
                     return False
                 seen.add(board[r][c])
                 # sudokovals[(i,j)] = val
@@ -34,13 +36,14 @@ class Solution:
                 for j in range(3):
                     row = (square // 3) * 3 + i
                     col = (square % 3) * 3 + j
+                    print("square - {}, row - {}, column - {}, value - {}".format(square, row, col, board[row][col]))
                     if board[row][col] == ".":
                         continue
                     if board[row][col] in seen:
                         print(seen)
-                        print("found repetition in the square ({}:{},{})".format(square, row,col))
+                        print("found repetition in the square - {}: row - {}, column - {}, value - {}".format(square, row, col,  board[row][col]))
                         return False
-                    seen.add(board[r][c])
+                    seen.add(board[row][col])
                     # sudokovals[(i,j)] = val
         
         return True
@@ -60,7 +63,8 @@ if __name__ == "__main__":
  [".",".",".","4","1","9",".",".","8"],
  [".",".",".",".","8",".",".","7","9"]]))
     
-    print(s.isValidSudoku([["1","2",".",".","3",".",".",".","."],
+    print(s.isValidSudoku([
+ ["1","2",".",".","3",".",".",".","."],
  ["4",".",".","5",".",".",".",".","."],
  [".","9","1",".",".",".",".",".","3"],
  ["5",".",".",".","6",".",".",".","4"],
