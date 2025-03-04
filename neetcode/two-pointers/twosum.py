ac654912516767a8a65b5fddb2929a0ffa9997f1
mahankali <vijayakumar.mahankali@gmail.com>
2025-03-03 23:51:39 -0800

    twosum problem using two-pointer approach

---
+++ neetcode/two-pointers/twosum.py
@@ -0,0 +1,17 @@
+class Solution(object):    
+    def twoSum(self, nums, target):
+        i, j, l = 0, 1, len(nums)
+        while i < l:
+            if nums[i] + nums[j] == target:
+                return [i, j]
+            else:
+                j += 1
+                if j == l:
+                    i += 1
+                    j = i+1
+        return None
+                
+s = Solution()
+print(s.twoSum([2,7,11,15], 9))
+print(s.twoSum([2,7,11,15], 26))
+print(s.twoSum([15,11,2,7], 26))
