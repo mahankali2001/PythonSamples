d47808461616d71b61ebbe259066548f104786cc
mahankali <vijayakumar.mahankali@gmail.com>
2025-03-09 00:05:09 -0800

    trap water pb - two pointer approach

--- neetcode/two-pointers/trapwater.py
+++ neetcode/two-pointers/trapwater.py
@@ -1,13 +1,30 @@
 class Solution(object):    
     def trapwater(self, height: list[int]) -> int:
-        i, l, ndic = 0, len(height), {}
-        for i, val in enumerate(height):
-            if i != 0 and i != l-1:
-                minval = min(max(height[:i]), max(height[i:])) 
-                ndic[i] = (lambda minval, val: 0 if minval-val < 0 else minval-val)(minval, val)
+        '''Two pointer approach'''
+        if not height:
+            return 0
+        n = len(height)
+        res = 0
 
-        print(ndic)
-        return sum(map(lambda item: item[1], ndic.items()))
+        for i in range(n):
+            leftMax = rightMax = height[i]
+
+            for j in range(i):
+                leftMax = max(leftMax, height[j])
+            for j in range(i + 1, n):
+                rightMax = max(rightMax, height[j])
+                
+            res += min(leftMax, rightMax) - height[i]
+        return res
+        
+        # i, l, ndic = 0, len(height), {}
+        # for i, val in enumerate(height):
+        #     if i != 0 and i != l-1:
+        #         minval = min(max(height[:i]), max(height[i:])) 
+        #         ndic[i] = (lambda minval, val: 0 if minval-val < 0 else minval-val)(minval, val)
+
+        # print(ndic)
+        # return sum(map(lambda item: item[1], ndic.items()))
 
 if __name__ == "__main__":
     s = Solution()
